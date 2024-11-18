e46f92cdd26b6f1a981cf37a0c81bc47af6bfd4c
ronesta <gvozzzd123@gmail.com>
2024-11-18 21:15:01 +0300

    Initial commit

---
+++ iTunesYapDatabase/iTunesYapDatabase/SearchHistoryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SearchHistoryViewController.swift
+//  iTunesYapDatabase
+//
+//  Created by Ибрагим Габибли on 18.11.2024.
+//
+
+import UIKit
+
+class SearchHistoryViewController: UIViewController {
+}
+
---
+++ iTunesYapDatabase/iTunesYapDatabase/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iTunesYapDatabase
+//
+//  Created by Ибрагим Габибли on 18.11.2024.
+//
+
+import UIKit
+import YapDatabase
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
