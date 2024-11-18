d395e1e5649a4441caab57a20e938f6c02556374
ronesta <gvozzzd123@gmail.com>
2024-11-18 21:23:30 +0300

    Create and initial setup SearchViewController, SearchHistoryViewController and AlbumViewController

--- iTunesYapDatabase/iTunesYapDatabase/SearchHistoryViewController.swift
+++ iTunesYapDatabase/iTunesYapDatabase/SearchHistoryViewController.swift
@@ -8,5 +8,71 @@
 import UIKit
 
 class SearchHistoryViewController: UIViewController {
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.separatorStyle = .singleLine
+        return tableView
+    }()
+    private let id = "cell"
+    var searchHistory = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        setupViews()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateSearchHistory()
+    }
+
+    private func setupNavigationBar() {
+        title = "History"
+    }
+
+    private func setupViews() {
+        view.addSubview(tableView)
+        view.backgroundColor = .systemGray6
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: id)
+
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    func updateSearchHistory() {
+        self.tableView.reloadData()
+    }
 }
+
+extension SearchHistoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        searchHistory.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: id, for: indexPath)
+        cell.textLabel?.text = searchHistory[indexPath.row]
+        return cell
+    }
+}
+
+extension SearchHistoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let selectedTerm = searchHistory[indexPath.row]
+        performSearch(for: selectedTerm)
+    }
+
+    func performSearch(for term: String) {
+        let searchViewController = SearchViewController()
+        searchViewController.searchAlbums(with: term)
+        navigationController?.pushViewController(searchViewController, animated: true)
+    }
+}
+
 
--- iTunesYapDatabase/iTunesYapDatabase/SearchViewController.swift
+++ iTunesYapDatabase/iTunesYapDatabase/SearchViewController.swift
@@ -6,13 +6,85 @@
 //
 
 import UIKit
-import YapDatabase
+import SnapKit
 
-class SearchViewController: UIViewController {
+final class SearchViewController: UIViewController {
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchBarStyle = .minimal
+        searchBar.placeholder = "Search Albums"
+        searchBar.sizeToFit()
+        return searchBar
+    }()
+
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 15, height: 130)
+        layout.minimumLineSpacing = 7
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsets.zero
+
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout
+        )
+        collectionView.backgroundColor = .clear
+        collectionView.register(
+            AlbumCollectionViewCell.self,
+            forCellWithReuseIdentifier: AlbumCollectionViewCell.id
+        )
+
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .systemGray6
+        view.addSubview(searchBar)
+        view.addSubview(collectionView)
+        navigationItem.titleView = searchBar
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+        }
+    }
+
+    func searchAlbums(with term: String) {
     }
 }
 
+// MARK: - UICollectionViewDataSource
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: AlbumCollectionViewCell.id,
+            for: indexPath)
+                as? AlbumCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension SearchViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        let albumViewController = AlbumViewController()
+        navigationController?.pushViewController(albumViewController, animated: true)
+    }
+}
